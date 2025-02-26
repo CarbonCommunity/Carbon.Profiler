47d8dc8276a4e31f8a208f723b1d74d5777281f5
raul <raulssorban@gmail.com>
2025-02-26 02:02:52 +0100

    One-lined that

--- src/HarmonyProfiler.cs
+++ src/HarmonyProfiler.cs
@@ -16,14 +16,7 @@ public sealed class HarmonyProfiler : IHarmonyModHooks
 		MonoProfilerConfig.Load(configPath);
 		InitNative();
 
-		if (MonoProfiler.Crashed)
-		{
-			Debug.LogError($"Carbon.Profiler crashed! (NATIVE_PROTOCOL:{MonoProfiler.NATIVE_PROTOCOL} MANAGED_PROTOCOL:{MonoProfiler.MANAGED_PROTOCOL})");
-		}
-		else
-		{
-			Debug.Log($"Carbon.Profiler initialized! (NATIVE_PROTOCOL:{MonoProfiler.NATIVE_PROTOCOL} MANAGED_PROTOCOL:{MonoProfiler.MANAGED_PROTOCOL})");
-		}
+		Debug.LogError($"Carbon.Profiler {(MonoProfiler.Crashed ? "crashed" : "initialized")}! (NATIVE_PROTOCOL:{MonoProfiler.NATIVE_PROTOCOL} MANAGED_PROTOCOL:{MonoProfiler.MANAGED_PROTOCOL})");
 	}
 
 	public void OnUnloaded(OnHarmonyModUnloadedArgs args)
